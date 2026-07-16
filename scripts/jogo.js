c8abdf2d67b9fd060ce80d7c8decafa3e2281abe
GAGABRIELTRUE <gabrielalmeidaale@gmail.com>
2026-07-16 18:01:42 -0300

    Update server URL in jogo.js

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -38,12 +38,9 @@ async function conectarSala() {
     if (!chave.startsWith("/")) chave = "/" + chave;
 
 
-    // Substitua "seu-projeto-backend" pelo subdomínio real que o Render gerou para você
-    const servidorHTTP = "https://onrender.com"; 
+        // Substitua "seu-projeto-backend" pelo subdomínio real que o Render gerou para você
+    const servidorHTTP = "https://whozap-server.onrender.com"; 
     const servidorURL = `wss://whozap-server.onrender.com${chave}`;
-
-
-
 
 
     
